703f4cf6bc58e3be86142556e9e124bfa2865490
掠秋之枫 <3062307383@qq.com>
2023-08-21 20:19:32 +0800

    Content And Source Commit

---
+++ BattleField/Source/BattleField/BattleFieldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleFieldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEFIELD_API ABattleFieldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
